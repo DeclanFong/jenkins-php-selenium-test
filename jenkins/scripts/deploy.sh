457388e4e6cc5353afdb4d8f811437ce09ed5158
DeclanFong <declanfongyiren@gmail.com>
2024-07-22 19:05:33 +0800

    please work

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\Users\\decla\\OneDrive\\Desktop\\SIT\\Year 2\\Y2T3\\Secure Software Development\\Labs\Week 8\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+
+# Update the path to use Unix-style slashes
+docker run -d -p 80:80 --name my-apache-php-app -v /home/decla/OneDrive/Desktop/SIT/Year_2/Y2T3/Secure_Software_Development/Labs/Week_8/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+
 sleep 1
+
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-

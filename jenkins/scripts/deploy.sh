d3d63b59e9f32e3a1ecc1ddf4469cd80ed88abfe
DeclanFong <declanfongyiren@gmail.com>
2024-07-23 09:21:00 +0800

    kkdss

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -9,7 +9,7 @@ if [ "$(docker ps -q -f name=my-apache-php-app)" ]; then
 fi
 
 # Run the new container
-docker run -d -p 80:80 --name my-apache-php-app -v \\c\\Users\\decla\\OneDrive\\Desktop\\SIT\\Year_2\\Y2T3\\Secure_Software_Development\\Labs\\Week_8\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/decla/OneDrive/Desktop/SIT/Year_2/Y2T3/Secure_Software_Development/Labs/Week_8/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 
 sleep 1
 

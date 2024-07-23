ebae05eeabd444142d63b18b126fb4c823296f41
DeclanFong <declanfongyiren@gmail.com>
2024-07-23 10:28:37 +0800

    please work

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -9,7 +9,7 @@ if [ "$(docker ps -q -f name=my-apache-php-app)" ]; then
 fi
 
 # Run the new container
-docker run -d -p 80:80 --name my-apache-php-app -v "//c/Users/decla/OneDrive/Desktop/SIT/Year_2/Y2T3/Secure_Software_Development/Labs/Week_8/jenkins-php-selenium-test:/var/www/html" php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v "/c/ssd/labs/jenkins-php-selenium-test:/var/www/html" php:7.2-apache
 
 # Ensure the correct permissions are set inside the container
 docker exec my-apache-php-app /bin/sh -c "chown -R www-data:www-data /var/www/html && find /var/www/html -type d -exec chmod 755 {} \; && find /var/www/html -type f -exec chmod 644 {} \;"
